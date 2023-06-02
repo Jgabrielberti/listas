bfa127d7e0d102354aa5e11c1479ea243d84fc29
João Gabriel Berti <127428856+Jgabrielberti@users.noreply.github.com>
2023-06-02 11:38:20 -0300

    Update lista.js

--- lista.js
+++ lista.js
@@ -1,6 +1,6 @@
 [
-  {"id":1, "nome":"Arthur", "idade":6},
-  {"id":2, "nome":"Alexandre", "idade":10},
-  {"id":3, "nome":"Georgia", "idade"45:},
-  {"id":4, "nome":"Luis Alexandre", "idade":60},
+  {"id":1, "Nome":"Arthur", "Idade":6},
+  {"id":2, "Nome":"Alexandre", "Idade":10},
+  {"id":3, "Nome":"Georgia", "Idade"45:},
+  {"id":4, "Nome":"Luis Alexandre", "Idade":60},
 ]
