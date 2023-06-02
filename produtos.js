8adb8985ff14f7493aba6f247337099fdd800cd3
João Gabriel Berti <127428856+Jgabrielberti@users.noreply.github.com>
2023-06-02 11:32:04 -0300

    Create produtos.js

---
+++ produtos.js
@@ -0,0 +1,6 @@
+[
+  {"id":1, "nome":"Mouse", "marca":"Razer", "preco":400}, 
+  {"id":2, "nome":"Notebook", "marca":"Acer", "preco":5000}, 
+  {"id":3, "nome":"Teclado", "marca":"Corsair", "preco":350}, 
+  {"id":4, "nome":"Celular", "marca":"Apple", "preco":3500},
+]  
